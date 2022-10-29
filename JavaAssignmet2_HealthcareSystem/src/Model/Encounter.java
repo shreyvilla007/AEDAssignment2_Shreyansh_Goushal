eef87994444b4639ea873d59a4acbe2300a98436
shreyvilla007 <goushalshreyansh@gmail.com>
2022-10-29 14:27:37 -0400

    10/29 2:27PM

--- JavaAssignmet2_HealthcareSystem/src/Model/Encounter.java
+++ JavaAssignmet2_HealthcareSystem/src/Model/Encounter.java
@@ -4,10 +4,38 @@
  */
 package Model;
 
+import java.util.Date;
+
 /**
  *
  * @author admin
  */
 public class Encounter {
-    
+    public Date visitDate;
+    public VitalSigns vitalSigns = new VitalSigns();
+    public Patient patient = new Patient();
+
+    public VitalSigns getVitalSigns() {
+        return vitalSigns;
+    }
+
+    public void setVitalSigns(VitalSigns vitalSigns) {
+        this.vitalSigns = vitalSigns;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public Date getVisitDate() {
+        return visitDate;
+    }
+
+    public void setVisitDate(Date visitDate) {
+        this.visitDate = visitDate;
+    }
 }
