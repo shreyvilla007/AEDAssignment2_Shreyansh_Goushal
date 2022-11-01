e78cf329990dcf45942985378387caf0db87b8cb
shreyvilla007 <goushalshreyansh@gmail.com>
2022-10-31 20:26:19 -0400

    10/31

--- JavaAssignmet2_HealthcareSystem/src/Model/HospitalDirectory.java
+++ JavaAssignmet2_HealthcareSystem/src/Model/HospitalDirectory.java
@@ -4,10 +4,32 @@
  */
 package Model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author admin
  */
 public class HospitalDirectory {
+    private ArrayList<Hospital> hospitals;
+
+    public HospitalDirectory(){
+        hospitals = new ArrayList<>();
+    }
     
+    public ArrayList<Hospital> getHospitals() {
+        return hospitals;
+    }
+
+    public void setHospitals(ArrayList<Hospital> hospitals) {
+        this.hospitals = hospitals;
+    }
+    
+    public void addHospital(Hospital hospital) {
+        this.hospitals.add(hospital);
+    }
+    
+    public void removeHospital(Hospital hospital) {
+        this.hospitals.remove(hospital);
+    }
 }
